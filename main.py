240b639dcffe2775ce1f39fef3fc72f9fe9e3b34
Kaifeng Yu <1536771545kai@gmail.com>
2025-04-05 17:37:44 -0400

    feat: complete run function

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ def run(filepath: str):
     INSERT DOCSTRING HERE
     """
     # open the json object
-    ...
+    with open(filepath, "r") as f:
+        data = json.load(f)
 
     # extract the reviews from the json file
-    ...
+    reviews = data["results"]
 
     # get a list of sentiments for each line using get_sentiment
-    ...
+    sentiments = get_sentiment(reviews)
 
     # plot a visualization expressing sentiment ratio
-    ...
+    make_plot(sentiments)
 
     # return sentiments
-    ...
+    return sentiments
 
 
 if __name__ == "__main__":
