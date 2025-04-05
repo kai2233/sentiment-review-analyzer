846ebf5e4ab97bd6bcfc64c7473df30e4788301d
Kaifeng Yu <1536771545kai@gmail.com>
2025-04-05 01:13:13 -0400

    feat: complete make_plot function

--- visualize.py
+++ visualize.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import matplotlib.pyplot as plt
 
 
@@ -5,4 +6,21 @@ def make_plot(sentiments: list) -> list:
     """
     INSERT DOCSTRING HERE
     """
+    sentiment_to_count = defaultdict(int)
     
+    for sen in sentiments:
+        sentiment_to_count[sen] += 1
+    
+    sentiment = []
+    count = []
+    for key, val in sentiment_to_count.items():
+        sentiment.append(key)
+        count.append(val)
+    plt.figure(figsize=(12,6))
+    plt.title("sentiments review")
+    fig, ax = plt.subplots()
+    ax.bar(sentiment, count)
+    ax.set_xlabel("count")
+    ax.set_ylabel("sentitments")
+
+    fig.savefig("./images/test.png")
