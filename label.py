7a9690fb31f9740599c36fb95e8ec99724b97ed1
Kaifeng Yu <1536771545kai@gmail.com>
2025-04-05 00:54:28 -0400

    feat: implement get_sentiment function

--- label.py
+++ label.py
@@ -1,8 +1,32 @@
 from openai import OpenAI
-
+from config import error_message
+import json
 
 def get_sentiment(text: list) -> list:
     """
     INSERT DOCSTRING HERE
     """
+    client = OpenAI()
+
+    if not text or any(isinstance(val,int) for val in text):
+        return error_message
     
+    system_prompt = """
+    You are a psychologist with 20 years of experience, you can easily detect the real meaning behind people's words.
+    """
+
+    prompt = f"""
+    For each line of text in the string below, please categorize the review
+    as either positive, neutral, negative, or irrelevant.
+
+    Use only a one-word response per line. Do not include any numbers.
+    """
+    result = client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = [
+            {"role":"developer", "content":system_prompt},
+            {"role":"user", "content":prompt}
+        ]
+    )
+    data = json.loads(result.choices[0].message.content)
+    return data["sentiments"]
